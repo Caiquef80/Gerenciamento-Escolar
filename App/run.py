e03468d04a4b714b8c0c9b6a09a5d82c3870b8dc
Jeff Rayner <jeff.rfsimoes@senacsp.edu.br>
2026-02-20 11:12:48 -0300

    feat: controle de login

--- App/run.py
+++ App/run.py
@@ -1,3 +1,17 @@
 from PyQt5.QtWidgets import QApplication
+from App.view.loginUI import LoginUI
+from App.controller.loginController import isLogged
 
 app = QApplication([])
+login = LoginUI()
+
+while not isLogged():
+    res = login.exec_()
+    if res:
+        print('login efetuado - carregar tela HOME')
+        # tela = MainUI()
+        # app.exec_()
+    else:
+        break
+
+print('programa encerrado')
